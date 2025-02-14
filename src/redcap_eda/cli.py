f8455e701dd10121ced1b1e3239da0657a7bbca4
Robert Portelli <github@robertportelli.com>
2025-02-14 01:26:36 +0000

    add logger.py to handle structured logging with rotating log handler and dynamic logging levels. Add logging support to the CLI.

--- src/redcap_eda/cli.py
+++ src/redcap_eda/cli.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
+import click
+from redcap_eda.logger import set_log_level
+
 """
 Command-line interface (CLI) for REDCap-EDA.
 """
 
-import click
-
 
 @click.group()
-def cli():
+@click.option("--debug", is_flag=True, help="Enable verbose debug logging")
+def cli(debug):
     """REDCap-EDA: Perform Exploratory Data Analysis on REDCap datasets."""
-    pass
+    set_log_level(debug)
 
 
 if __name__ == "__main__":
